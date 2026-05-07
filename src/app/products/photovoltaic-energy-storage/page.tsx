e4ae19d2f5f55d0331ca52463d79cfbfabb2e15d
jenish <prajapatijenish37@gmail.com>
2026-05-07 12:50:55 +0545

    fix bug

--- src/app/products/photovoltaic-energy-storage/page.tsx
+++ src/app/products/photovoltaic-energy-storage/page.tsx
@@ -1,5 +1,5 @@
-// import { redirect } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
-// export default function Page() {
-//   redirect('/products?category=pv-storage');
-// }
+export default function Page() {
+  redirect('/products?category=pv-storage');
+}
